df61667affa5cda3947dc0c9b9881f57382c8b93
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-08-01 08:24:14 +0700

    update

--- C2312L/PHP/blog-app/bootstrap/app.php
+++ C2312L/PHP/blog-app/bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(CheckSession::class);
+        //$middleware->append(CheckSession::class);
+        $middleware->use([            
+           // CheckSession::class,            
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
