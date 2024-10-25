aa189c4059b9f0d87eb10447d054600b2aaac600
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-10-25 09:55:54 +0700

    update

---
+++ C2408G/Semester1/EPC/25-10-2024/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define max 5;
+//int max = 5;
+
+int main(int argc, char *argv[]) {
+	
+	int a=10,b=20;
+   char x=1,y=0;
+   if(a,b,x,y)
+   {
+      printf("EXAM");
+   }
+
+}
