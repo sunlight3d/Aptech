98719286bffd94ec6566a35edaf8e2350c0b7e4f
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-10-28 09:27:52 +0700

    update

--- C2408G/Semester1/EPC/28-10-2024/main.c
+++ C2408G/Semester1/EPC/28-10-2024/main.c
@@ -8,9 +8,9 @@ struct Book {
 };
 void displayBook(struct Book book) {
 	printf("Title : %s, ", book.title);
-	printf("Title : %s, ", book.author);
-	printf("Title : %d, ", book.publicationYear);
-	printf("Title : %.2f, ", book.price);
+	printf("author : %s, ", book.author);
+	printf("publicationYear : %d, ", book.publicationYear);
+	printf("price : %.2f\n", book.price);
 }
 int main(int argc, char *argv[]) {
 	struct Book books[10]; 
@@ -18,19 +18,22 @@ int main(int argc, char *argv[]) {
 	int currentIndex = 0;
 	int choice = 0;
 	int i;
+	struct Book newBook;
+	struct Book* foundBook;
+	char title[100];			
 	do {
-		printf("Add a new book");
-		printf("Display all books");
-		printf("Update book price by title");
-		printf("Quit");
+		printf("1. Add a new book\n");
+		printf("2. Display all books\n");
+		printf("3. Update book price by title\n");
+		printf("4. Quit\n");
+		printf("Enter your choice(1-4) : \n");scanf("%d", &choice);
 		switch (choice)
 		{
-		case 1:
-			struct Book newBook;
+		case 1:			
 			printf("Enter title : ");scanf(" %[^\n]", newBook.title); 
 			printf("Enter author : ");scanf(" %[^\n]", newBook.author); 
-			printf("Enter publicationYear : ");scanf("%d", newBook.publicationYear); 
-			printf("Enter price : ");scanf("%f", newBook.price); 
+			printf("Enter publicationYear : ");scanf("%d", &newBook.publicationYear); 
+			printf("Enter price : ");scanf("%f", &newBook.price); 
 			books[currentIndex] = newBook;
 			currentIndex++;
 			break;
@@ -41,12 +44,17 @@ int main(int argc, char *argv[]) {
 			
 			break;
 		case 3:
-			//find a book with title(input from keyboard)
-			char title[100];
-			struct Book foundBook;
+			//find a book with title(input from keyboard)			
 			printf("which book do you like to search ?");scanf(" %[^\n]", title);
 			for(i = 0; i < currentIndex; i++) {
-				displayBook(books[i]);
+				if(strcmp(books[i].title, title) == 0) {
+					foundBook = &books[i];
+					//update that book
+					printf("Enter title : ");scanf(" %[^\n]", foundBook->title); 
+					printf("Enter author : ");scanf(" %[^\n]", foundBook->author); 
+					printf("Enter publicationYear : ");scanf("%d", &foundBook->publicationYear); 
+					printf("Enter price : ");scanf("%f", &foundBook->price); 
+				} 
 			}
 
 			//update that book
