b0eb3c46523741c26e9718cd8bb4b87d55d9137c
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-12-09 21:13:19 +0700

    update

--- C2409LDoican/HTML5/09-12-2024/nextpage.js
+++ C2409LDoican/HTML5/09-12-2024/nextpage.js
@@ -1,4 +1,28 @@
+const klassNames = ['C1234L', 'C2409G', 'C2234L']
+let klassSelectTag = document.getElementById("klass");
 
+function addKlass(){
+    let newKlass = prompt(`Enter class"s name :`)
+    klassNames.push(newKlass)
+    debugger
+    reloadKlasses()
+}
+
+/*
+let option = document.createElement("option");
+option.text = "C1234L";
+option.value = "C1234L"; 
+*/
+function reloadKlasses(){
+    klassSelectTag.innerText = null;
+    for(let i = 0; i < klassNames.length; i++) {
+        let option = document.createElement("option");
+        option.text = klassNames[i];
+        option.value = klassNames[i]; 
+        klassSelectTag.add(option);
+    }
+}
+reloadKlasses()
 function addStudent() {
     const name = document.getElementById('name')?.value?.trim()
     const dob = document.getElementById('dob')?.value?.trim()
