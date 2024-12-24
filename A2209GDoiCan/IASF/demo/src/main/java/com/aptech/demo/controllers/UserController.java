d7f7c069475e11b64093fc900e53c772530a2fb7
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-12-24 09:26:01 +0700

    update

--- A2209GDoiCan/IASF/demo/src/main/java/com/aptech/demo/controllers/UserController.java
+++ A2209GDoiCan/IASF/demo/src/main/java/com/aptech/demo/controllers/UserController.java
@@ -5,13 +5,17 @@ import com.aptech.demo.models.Student;
 import com.aptech.demo.models.User;
 import com.aptech.demo.repositories.StudentRepository;
 import com.aptech.demo.repositories.UserRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.aptech.demo.dtos.requests.*;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.security.MessageDigest;
@@ -31,8 +35,15 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    //http://localhost:8082/http://localhost:8082/users/register/register
-    public String register(@ModelAttribute("request") RegisterUserRequest request,  Model model) {
+    //http://localhost:8082/http://localhost:8082/users/register
+    public String register(
+            @ModelAttribute("request") @Valid RegisterUserRequest request,
+            BindingResult bindingResult,
+            Model model) {
+        // Nếu có lỗi validation, trả lại view với lỗi
+        if (bindingResult.hasErrors()) {
+            return "users/register";
+        }
         //convert InsertStudentRequest to model
         User user = User.fromRequest(request);
         repository.save(user);
@@ -48,10 +59,16 @@ public class UserController {
     }
     @PostMapping("/login")
     //http://localhost:8082/http://localhost:8082/users/register/register
-    public String login(@ModelAttribute("request") LoginUserRequest request,  Model model) {
+    public String login(
+            @ModelAttribute("request") @Valid LoginUserRequest request,
+            BindingResult bindingResult,
+            Model model) {
+        if (bindingResult.hasErrors()) {
+            return "users/login";
+        }
         //Student student = studentRepository.findById(studentId).orElseThrow(() -> new RuntimeException("Student not found"));
         User existingUser = repository.findByEmail(request.getEmail()).orElseThrow(() -> new RuntimeException("User not found"));
-        if (existingUser.getPassword().equals(request.getPassword())) {
+        if (!existingUser.getPassword().equals(request.getPassword())) {
             //not recommended !!
             throw new RuntimeException("Wrong email/password");
         }
