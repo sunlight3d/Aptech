b48da2d33284a548ecf6a4c869c0989f4b1b2bf1
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-12-25 20:51:05 +0700

    update

--- C2409LDoican/HTML5/25-12-2024/index.js
+++ C2409LDoican/HTML5/25-12-2024/index.js
@@ -2,6 +2,9 @@ let registerButton = document.getElementById('registerButton');
 let users = [
     
 ]
+function checkEmail(email) {
+    return email.match(/^[a-zA-Z0-9]+@[A-Za-z]+\.[a-zA-Z]{3}/g) ?.length > 0;
+}
 function register(event) {
     debugger
     let fullname = document.getElementById('fullname').value;
@@ -10,6 +13,26 @@ function register(event) {
     let phoneNumber = document.getElementById('phoneNumber').value;
     let password = document.getElementById('password').value;
     let confirmPassword = document.getElementById('confirmPassword').value;
-    let gender = document.querySelector('input[name="rate"]:checked').value;
+    let gender = document.querySelector('input[name="gender"]:checked')?.value;
+    
+    if(!checkEmail(email)) {
+        alert('Email must be in correct format')
+        return;
+    }
+    //validate
+    if(password !== confirmPassword) {
+        alert('Password and confirm password must be the same')
+        return;
+    }
+
+    users.push({
+        fullname,
+        username,
+        email,
+        phoneNumber,
+        password,
+        gender
+    })
+    alert(JSON.stringify(users))
 }
 registerButton.addEventListener("click", register);
