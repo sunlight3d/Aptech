54d4a10436e72d3e3834ceeec33489dee3b5781e
Nguyen Duc Hoang <sunlight4d@gmail.com>
2025-02-05 11:15:55 +0700

    update

--- C2209GDoiCan/ADFD/myapp/lib/config.dart
+++ C2209GDoiCan/ADFD/myapp/lib/config.dart
@@ -2,5 +2,9 @@
 //const String API_BASE_URL = 'http://10.0.3.125:3000/api';
 //const String API_BASE_URL = 'http://192.168.1.2:3000/api';
 //const String API_BASE_URL = 'http://192.168.1.9:3000/api';
-const String API_BASE_URL = 'http://192.168.1.15:3000/api';
+//const String API_BASE_URL = 'http://192.168.1.15:3000/api';//del-optiplex
+const String API_BASE_URL = 'http://10.0.0.245:3000/api';//del-optiplex
+
+//healthcheck: http://10.0.0.245:3000/api/healthcheck
+
 
